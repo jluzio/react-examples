178b4dc079e12143b76ea1f07bf325adbe169843
João Luzio <joao.luzio@gmail.com>
2018-06-29 19:29:35 +0100

    Js E6 tests

--- react-app/src/tutorials/tic-tac-toe/game.js
+++ react-app/src/tutorials/tic-tac-toe/game.js
@@ -1,7 +1,36 @@
 import React, { Component } from 'react';
 import Board from './board';
 
+/*
+class GameState {
+    constructor(squares, xIsNextMove, winner) {
+        this.squares = squares;
+        this.xIsNextMove = xIsNextMove;
+        this.winner = winner;
+    }
+}
+*/
+
 export default class Game extends Component {
+    /*
+    constructor(props) {
+        super(props);
+
+        let newGameState = this.createNewGameState();
+        this.state = {
+            history: [ newGameState ],
+        }
+    }
+
+    createNewGameState() {
+        return new GameState(
+            Array(9).fill(null),
+            true,
+            null
+        );
+    }
+    */
+
     render() {
         return (
             <div className="tutorial">
--- react-app/src/tutorials/tutorial-list.js
+++ react-app/src/tutorials/tutorial-list.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Game from './tic-tac-toe/game';
 import CounterExamples from './state/counter-examples';
+import JsE6Test from './js-e6/js-e6-test';
 
 export default class TutorialList extends Component {
     render() {
@@ -13,7 +14,7 @@ export default class TutorialList extends Component {
                         <div className="card-header" id="headingTicTacToe">
                             <h5 className="mb-0">
                                 <button className="btn btn-link" data-toggle="collapse" data-target="#collapseTicTacToe" aria-expanded="true" aria-controls="collapseTicTacToe">
-                                    TicTacToe
+                                    Tic-Tac-Toe
                                 </button>
                             </h5>
                         </div>
@@ -34,6 +35,20 @@ export default class TutorialList extends Component {
                         <div id="collapseState" className="collapse" aria-labelledby="headingState" data-parent="#accordion">
                             <div className="card-body">
                                 <CounterExamples />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card tutorial-item">
+                        <div className="card-header" id="headingJsE6">
+                            <h5 className="mb-0">
+                                <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseJsE6" aria-expanded="false" aria-controls="collapseJsE6">
+                                    Js E6
+                                </button>
+                            </h5>
+                        </div>
+                        <div id="collapseJsE6" className="collapse" aria-labelledby="headingJsE6" data-parent="#accordion">
+                            <div className="card-body">
+                                <JsE6Test />
                             </div>
                         </div>
                     </div>
