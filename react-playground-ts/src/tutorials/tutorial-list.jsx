97f7636c3937be6959a82b3dae68daa3b4777f1d
João Luzio <joao.luzio@gmail.com>
2020-01-10 19:08:45 +0000

    Navbar example

--- react-playground-ts/src/tutorials/tutorial-list.jsx
+++ react-playground-ts/src/tutorials/tutorial-list.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import Game from './tic-tac-toe/game'
 import CounterExamples from './state/counter-examples'
 import './tutorial.css'
@@ -10,6 +13,29 @@ export default class TutorialList extends React.Component {
     return (
       <div className="tutorial-list">
         <h2>Tutorials</h2>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <div id="accordion">
           <div className="card tutorial-item">
             <div className="card-header" id="headingTicTacToe">
