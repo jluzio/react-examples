dc27757cb3e870c8808ffbdb156c52aace7b07af
João Luzio <joao.luzio@gmail.com>
2020-03-06 15:53:56 +0000

    examples

--- react-playground-ts/src/components/examples/render-props/RenderPropsExample.tsx
+++ react-playground-ts/src/components/examples/render-props/RenderPropsExample.tsx
@@ -16,16 +16,17 @@ interface CatProps {
 class Cat extends React.Component<CatProps> {
   render() {
     const { mouse } = this.props
+    const size = 32
     return (
       <img
         src={logo}
         alt="Cat"
         style={{
           position: 'absolute',
-          left: mouse.x,
-          top: mouse.y,
-          height: '32px',
-          width: '32px'
+          left: mouse.x - size / 2,
+          top: mouse.y - size / 2,
+          height: `${size}px`,
+          width: `${size}px`
         }}
       />
     )
@@ -47,32 +48,40 @@ class Mouse extends React.Component<MouseProps, MouseState> {
     this.state = { x: 0, y: 0 }
   }
 
-  handleMouseMove(event: React.MouseEvent) {
+  handleMouseMove(event: React.MouseEvent<HTMLDivElement>) {
     // const target = event.target
     // TODO: remove offsets from parent
+    const { currentTarget } = event
+    const parentPos = currentTarget.getBoundingClientRect()
     this.setState({
-      x: event.clientX,
-      y: event.clientY
+      x: event.clientX - parentPos.x,
+      y: event.clientY - parentPos.y
     })
   }
 
   render() {
     const { render } = this.props
+    const { x, y } = this.state
     return (
-      <div
-        style={{
-          height: '100%',
-          border: '1px solid green'
-          // position: 'relative'
-        }}
-        onMouseMove={this.handleMouseMove}
-      >
-        {/*
+      <>
+        <div
+          style={{
+            height: '100%',
+            border: '1px solid green',
+            position: 'relative'
+          }}
+          onMouseMove={this.handleMouseMove}
+        >
+          {/*
           Instead of providing a static representation of what <Mouse> renders,
           use the `render` prop to dynamically determine what to render.
         */}
-        {render(this.state)}
-      </div>
+          {render(this.state)}
+          <p>
+            x: {x}, y: {y}
+          </p>
+        </div>
+      </>
     )
   }
 }
